0593f936259e7c5c7d50e67eaf5b9014899f1857
Joel Encarnacion <joelencarnacion07@gmail.com>
2025-01-17 17:45:46 -0400

    install and configure react-big-calendar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,53 @@
+import { addHours } from 'date-fns';
+import { Calendar } from 'react-big-calendar';
+
+import { localizer } from '../../helpers';
 import { Navbar } from '../components';
 
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const myEventsList = [
+	{
+		title: `Joel's birthday`,
+		notes: 'We have to buy the cake',
+		start: new Date(),
+		end: addHours(new Date(), 2),
+		bgColor: '#fafafa',
+		user: {
+			_id: '123',
+			name: 'Joel',
+		},
+	},
+];
+
 export const CalendarPage = () => {
+	const eventStyleGetter = (event, start, end, isSelected) => {
+		console.log({ event, start, end, isSelected });
+
+		const style = {
+			backgroundColor: '#347CF7',
+			borderRadius: '0px',
+			opacity: 0.8,
+			color: 'white',
+		};
+
+		return {
+			style,
+		};
+	};
+
 	return (
 		<>
 			<Navbar />
+
+			<Calendar
+				localizer={localizer}
+				events={myEventsList}
+				startAccessor='start'
+				endAccessor='end'
+				style={{ height: 500 }}
+				eventPropGetter={eventStyleGetter}
+			/>
 		</>
 	);
 };
